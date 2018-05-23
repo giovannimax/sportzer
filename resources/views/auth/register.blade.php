3c1beb99658d1ccf90b8132999a6d7c4d14db6a9
giovannimax <mindlesh13@gmail.com>
2018-05-23 20:07:22 +0800

    Updated registration view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,86 +1,105 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Unplug - Building Real Human Connection</title>
+	<title>Sportzer</title>
 	<link rel="stylesheet" type="text/css" href="css/material-design-icons.css">
+	<link rel="stylesheet" type="text/css" href="css/app.css">
 	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
 	<script type="text/javascript" src="js/jquery.min.js"></script>
-	<script type="text/javascript" src="js/materialize.js"></script>
-	<style type="text/css">
-	html,body{
-		margin: 0px;
-		overflow: hidden;
-		font-family: "Nunito Sans" !important;
-	}
-	@font-face {
-  	font-family: 'Nunito Sans';
-  	font-style: normal;
-  	font-weight: 400;
-  	src: local('Nunito Sans Regular'), local('NunitoSans-Regular'), url(fonts/nunito_sans.woff2) format('woff2');
-  	unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-	}
-		.mywrapper{
-			font-family: "Nunito Sans";
-			width: 100%;
-			background: url(img/signupbg.jpg);
-			background-repeat: no-repeat;
-			height: 100vh;
-			padding-top: 75px;
-			background-size: cover;
-		}
-
-		.regcontainer{
-			margin:auto;
-			width: 30%;
-			background: white;
-			padding: 30px; 
-			text-align: center;
-		}
-
-		.txtbox{
-			font-family: "Nunito Sans";
-			font-weight: 900;
-			margin-top: 20px;
-			padding: 10px;
-			width: 100%;
-		}
-		.btn-primary{
-			border-radius: 0px !important;
-			margin-top: 30px;
-			width: 100%;
-		}
-		textarea {
-   			 resize: none;
-   			 height: 200px;
-		}
-
-		.gendercont{
-			text-align: left !important;
-		}
-	</style>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
 </head>
 <body>
-	<div class="mywrapper">
-		
-		<div class="regcontainer">
+	<div class="mytwowrapper">
+		<div class="row">
+			<div class="col s6">
+				<div style="width:100%; background: rgba(0,0,0,0.2); height: 90vh; padding: 45px;">
+					<img style="width: 80%;" src="images/sportzer_cropped_white.png"><br>
+					<p style="color: white; font-weight: 500; text-align: justify;">
+						Sportzer aims to solve the problem which Organizer usually encountered during creating an event. Sportzer is a system which is used in the economy specifically in the Sports Area industry that connects organizers to participants online and gives them a satisfying service upon entering a certain sports event. 
+					</p>
+				</div>
+			</div>
+			<div class="col s6">
+					<!--regcontainer-->
+
+		<div class="regtwocontainer">
 			<h3>Join us!</h3>
 			<form method="POST">
-				<input type="text" name="name" placeholder="Name" class="txtbox"><br/>
-				<input type="email" name="email" placeholder="Email" class="txtbox"><br/>
-				<input type="password" name="pass" placeholder="Password" class="txtbox"><br>
-				<div class="gendercont">
-					<label>Gender</label>
-					<p>
-				      <input class="with-gap" name="gender" type="radio" id="male" value="male" />
-				      <label for="male">Male</label>
-   						&nbsp;&nbsp;&nbsp;					
-				      <input class="with-gap" name="gender" type="radio" id="female" value="female" />
-				      <label for="female">Female</label>
-   					</p>
+			<div class="row">
+				<div class="input-field col s6">
+				<input placeholder="John" id="first_name" type="text" class="validate">
+				<label for="first">First Name</label>
 				</div>
+				<div class="input-field col s6">
+				<input placeholder="Doe" id="last_name" type="text" class="validate">
+				<label for="last_name">Last Name</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s6">
+					<input placeholder="January 01, 2018" id="bdate" type="text" class="datepicker">
+					<label for="bdate">Birthdate</label>
+				</div>
+				<div class="input-field col s6">
+					<input placeholder="+639123465789" id="pnumber" type="number" class="validate">
+					<label for="pnumber">Phone Number</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s12">
+					<input placeholder="Jordan Creek Parkwa, Des Moines, Iowa" id="address" type="text" class="validate">
+					<label for="address">Address</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s12">
+					<input placeholder="sample@example.com" id="email" type="email" class="validate">
+					<label for="email">Email</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s12">
+					<input placeholder="*******" id="password" type="password" class="validate">
+					<label for="password">Password</label>
+				</div>
+			</div>
+			<div class="row">
+				<div class="input-field col s12">
+					<input placeholder="*******" id="password" type="password" class="validate">
+					<label for="password">Verify Password</label>
+				</div>
+			</div>
+				<div class="gendercont">
+					<label>Gender</label><br>
+
+					<p>
+					    <label>
+					      <input class="with-gap" name="gender" id="male" type="radio" value="male" checked />
+					      <span>Male</span>
+					    </label>&nbsp;&nbsp;&nbsp;&nbsp;
+					    <label>
+					      <input class="with-gap" name="gender" id="female" type="radio" value="female" checked />
+					      <span>Female</span>
+					    </label>
+					  </p>
+				</div>
+			
+					<!-- //<label>Gender</label> -->
 				<input name="register" type="submit" value="Register" class="waves-effect btn btn-primary"/>
 			</form>
 		</div>
+
+					<!--end regcontainer-->
+			</div>
+		</div>
 	</div>
+</div>
+	<script>
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.datepicker').datepicker();
+  });
+	</script>
 </body>
 </html>
