9e0e6fef2e525326d65ae88fba80b5e8afed1e6b
Mitch Alforque <malforque717@gmail.com>
2018-05-26 05:02:48 +0800

    change title to sportzer

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
+        <link rel="shortcut icon" href="images/sportzer_icon.png" type="image/x-icon">
+        <title>Sportzer</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Sportzer
                 </div>
 
                 <div class="links">
