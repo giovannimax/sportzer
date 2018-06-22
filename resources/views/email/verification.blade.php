480e6327ceff40e36c1bf1c4aea9b15484dfb55b
Mitch Alforque <malforque717@gmail.com>
2018-06-22 23:47:50 +0800

    Partial layout for verification view

--- resources/views/email/verification.blade.php
+++ resources/views/email/verification.blade.php
@@ -1,1 +1,30 @@
-You have successfully registered. An email has been sent to you for verification
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+
+<title>Sportzer</title>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+
+<!-- Fonts -->
+<link rel="dns-prefetch" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-primary">
+    <div class="verification-container">
+        <div class="verification-image">
+            <div class="logo">
+            <img class="image" src="images/sportzer_cropped_white.png" alt="">
+            </div>
+        </div>
+        <div class="verification-description"></div>
+    </div>
+</body>
+</html>
