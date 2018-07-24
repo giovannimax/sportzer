470173899d60982b157a34146c0bf6b2d65ac770
Mitch Alforque <malforque717@gmail.com>
2018-07-25 00:26:34 +0800

    Added function to create event

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Event;
+use Carbon\Carbon;
 class EventController extends Controller
 {
     //
+    public function create(Request $request){
+        $event = new Event();
+        if($request->filled(['startDate', 'endDate'])){
+            $request->merge([
+                'startDate' => Carbon::parse($request->startDate)->format('Y-m-d'),
+                'endDate' => Carbon::parse($request->endDate)->format('Y-m-d'),
+                ]);
+        }
+        $event->addEvent($request->except('_token'));
+    }
 }
