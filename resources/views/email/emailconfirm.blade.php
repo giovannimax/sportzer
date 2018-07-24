69fe954e3e026d668c24a112e87920bcd0293b9a
Mitch Alforque <malforque717@gmail.com>
2018-07-24 23:41:16 +0800

    Removed static data in email confirmation

--- resources/views/email/emailconfirm.blade.php
+++ resources/views/email/emailconfirm.blade.php
@@ -12,7 +12,7 @@
            Your email have successfully verified. 
            <div class="emailcont">
            		{{session('email')}}
-           		midnlesh13@gmal.com  <i class="material-icons red-text" style="margin-left: 20px; vertical-align: middle;">check</i>
+           		<i class="material-icons red-text" style="margin-left: 20px; vertical-align: middle;">check</i>
            </div>
            <br> Click here to <a href="{{route('login')}}">Login</a>
 		</div>
