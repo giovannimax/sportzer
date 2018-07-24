9aa5289041550ea274389cf7d33f065e085b4c53
Mitch Alforque <malforque717@gmail.com>
2018-07-24 23:41:56 +0800

    Created events table to database

---
+++ database/migrations/2018_07_24_152448_create_events_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('category');
+            $table->longText('description');
+            $table->date('startDate');
+            $table->date('endDate');
+            $table->string('venue');
+            $table->integer('status')->default(1);
+            $table->bigInteger('expectedParticipants');
+            $table->bigInteger('totalParticipants');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
