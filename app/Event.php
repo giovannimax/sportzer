97f07c093b7f462dd79fa19596b9304bd3c96107
Mitch Alforque <malforque717@gmail.com>
2018-07-25 00:26:21 +0800

    Added add event function and fillable fields

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
+
+    protected $fillable = [
+        'name',
+        'category',
+        'description',
+        'startDate',
+        'endDate',
+        'venue',
+        'expectedParticipants',
+    ];
     //
+    public function addEvent($data){
+        return $this->create($data);
+    }
 }
