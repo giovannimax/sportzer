3e2a2610ea65dbcd26e73b1212c0f3c50d7eb035
Mitch Alforque <malforque717@gmail.com>
2018-07-25 01:22:28 +0800

    Display data from database

--- resources/views/organizer/events/events.blade.php
+++ resources/views/organizer/events/events.blade.php
@@ -2,14 +2,21 @@
 @extends('layouts.main')
 
 @section('content')
-
 <div class="row">
+@if(\Session::has('success'))
+<div class="card light-blue lighten-5">
+    <div class="card-content light-blue-text">
+      <p>Event added successfully</p>
+    </div>
+ </div>
+@endif
   <div class="col s12 m6 l6">
      <a href="events/create" class="waves-effect waves-dark btn light-blue lighten-1 white-text btncreateevent"><i class="material-icons" style="vertical-align:bottom;margin-right: 5px;">add</i>New Event</a>
   </div>
-  
 </div>
 <div class="row">
+@if(!$eventList->isEmpty())
+  @foreach($eventList as $events)
   <div class="col s12 m6 l4" style="padding: 10px;">
      <div class="event-card hoverable">
        <div class="eventpiccont">
@@ -19,17 +26,27 @@
        </div>
        <div class="eventlabel">
         <div>
-         <font>Event Name</font>
+         <font>{{ $events->name }}</font>
        </div>
           <div class="lowerlabel">
-           <font><i class="material-icons eventlocicon">location_on</i>Sanciangko St., Cebu City</font>
+           <font><i class="material-icons eventlocicon">location_on</i>{{ $events->venue }}</font>
           
-             <font class="eventongoing">Ongoing</font>
+             <font class="eventongoing">{{ $events->status === 1 ? 'Ongoing' : 'Ended' }}</font>
          </div>
        </div>
      </div>
   </div>
-   <div class="col s12 m6 l4" style="padding: 10px;">
+  @endforeach
+  @else
+  <div class="d-flex justify-content-center">
+    <div>
+      <h3>
+      No Events Available as of now
+      </h3>
+    </div>
+  </div>
+  @endif
+   <!-- <div class="col s12 m6 l4" style="padding: 10px;">
      <div class="event-card hoverable">
        <div class="eventpiccont">
           <div class="eventpic" style="background: url('/../images/gallary/13.jpg');background-size:cover;background-position:center;">
@@ -104,7 +121,7 @@
          </div>
        </div>
      </div>
-  </div>
+  </div> -->
 </div>
 
 @endsection
