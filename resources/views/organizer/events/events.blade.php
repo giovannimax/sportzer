d9bb9474cb8af0e83cf001e5665f6522b937c68d
Mitch Alforque <malforque717@gmail.com>
2018-07-25 23:53:20 +0800

    Removed comments

--- resources/views/organizer/events/events.blade.php
+++ resources/views/organizer/events/events.blade.php
@@ -48,82 +48,15 @@
     </div>
   </div>
   @endif
-   <!-- <div class="col s12 m6 l4" style="padding: 10px;">
-     <div class="event-card hoverable">
-       <div class="eventpiccont">
-          <div class="eventpic" style="background: url('/../images/gallary/13.jpg');background-size:cover;background-position:center;">
-            
-          </div>
-       </div>
-       <div class="eventlabel">
-        <div>
-         <font>Event one</font>
-       </div>
-          <div class="lowerlabel">
-           <font><i class="material-icons eventlocicon">location_on</i>Colon St., Cebu City</font>
-          
-             <font class="eventsoon">Soon</font>
-         </div>
-       </div>
-     </div>
-  </div> 
-  <div class="col s12 m6 l4" style="padding: 10px;">
-     <div class="event-card hoverable">
-       <div class="eventpiccont">
-          <div class="eventpic" style="background: url('/../images/gallary/2.jpg');background-size:cover;background-position:center;">
-            
-          </div>
-       </div>
-       <div class="eventlabel">
-        <div>
-         <font>Event two</font>
-       </div>
-          <div class="lowerlabel">
-           <font><i class="material-icons eventlocicon">location_on</i>Talisay City, Cebu</font>
-          
-             <font class="eventdone">Done</font>
-         </div>
-       </div>
-     </div>
-  </div>
-  <div class="col s12 m6 l4" style="padding: 10px;">
-     <div class="event-card hoverable">
-       <div class="eventpiccont">
-          <div class="eventpic" style="background: url('/../images/gallary/11.jpg');background-size:cover;background-position:center;">
-            
-          </div>
-       </div>
-       <div class="eventlabel">
-        <div>
-         <font>Event three</font>
-       </div>
-          <div class="lowerlabel">
-           <font><i class="material-icons eventlocicon">location_on</i>Poblacion,Talisay City, Cebu</font>
-          
-             <font class="eventdone">Done</font>
-         </div>
-       </div>
-     </div>
-  </div>
-  <div class="col s12 m6 l4" style="padding: 10px;">
-     <div class="event-card hoverable">
-       <div class="eventpiccont">
-          <div class="eventpic" style="background: url('/../images/gallary/8.jpg');background-size:cover;background-position:center;">
-            
-          </div>
-       </div>
-       <div class="eventlabel">
-        <div>
-         <font>Event four</font>
-       </div>
-          <div class="lowerlabel">
-           <font><i class="material-icons eventlocicon">location_on</i>Cebu Business Park, Cebu City</font>
-          
-             <font class="eventdone">Done</font>
-         </div>
-       </div>
-     </div>
-  </div> -->
 </div>
 
 @endsection
+@push('scripts')
+  <script>
+    $(function(){
+      openModal = () => {
+        alert('hello');
+      }
+    });
+  </script>
+@endpush
