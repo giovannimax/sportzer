81ee2952d1320c7a1f11773d32e8adc6cf459de4
Mitch Alforque <malforque717@gmail.com>
2018-08-02 23:29:06 +0800

    Added category and sports seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(UserTableSeeder::class);
+        $this->call(CategorySeeder::class);
+        $this->call(SportsSeeder::class);
     }
 }
